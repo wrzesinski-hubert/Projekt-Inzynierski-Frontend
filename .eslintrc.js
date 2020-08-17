324b83eecb3a74541f8d037ece7f69749da7a3b4
Maciek Głowacki <glowackimaciej97@gmail.com>
2020-08-17 19:21:14 +0200

    added linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
