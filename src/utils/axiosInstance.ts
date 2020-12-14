39a7e109c8dc4097432a92386dba7621472c5a83
Maciek Głowacki <glowackimaciej97@gmail.com>
2020-12-14 17:20:44 +0100

    increased delay

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -5,6 +5,7 @@ export const axiosInstance = axios.create();
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('userToken');
+
     config.headers['Content-Type'] = 'application/json';
     config.headers['Authorization'] = token ? `Bearer ${token}` : '';
     return config;
